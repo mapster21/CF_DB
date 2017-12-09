2607fa2a3383cdc623e1b02e71e588f0856f2eb0
mapster21 <33132185+mapster21@users.noreply.github.com>
2017-12-09 22:02:12 +0000

    Add files via upload

---
+++ test_simple_with_object.py
@@ -0,0 +1,34 @@
+import unittest
+from process_changes_with_object import get_commits, read_file
+import pandas as pd
+import numpy as np
+# Unit test for data frame equality
+np.random.seed(30)
+df_old = pd.DataFrame(np.random.randn(4,5))
+df_old
+np.random.seed(30)
+df_new = pd.DataFrame(np.random.randn(4,5))
+df_new
+df_old.equals(df_new) #Equality check here, returns boolean expression: True/False
+
+class TestCommits(unittest.TestCase):
+
+    def setUp(self):
+        self.data = read_file('changes_python.log')
+
+    def test_number_of_lines(self):
+        self.assertEqual(5255, len(self.data))
+
+    def test_number_of_commits(self):
+        commits = get_commits(self.data)
+        self.assertEqual(422, len(commits))
+        self.assertEqual('Thomas', commits[0].author)
+        self.assertEqual('Jimmy', commits[420].author)
+        self.assertEqual(['FTRPC-500: Frontier Android || Inconsistencey in My Activity screen',
+                'Client used systemAttribute name="Creation-Date" instead of versionCreated as version created.'],
+                commits[24].comment)
+        self.assertEqual(['M /cloud/personal/client-international/android/branches/android-15.2-solutions/libs/model/src/com/biscay/client/android/model/util/sync/dv/SyncAdapter.java'],
+                commits[20].changed_path)
+        
+if __name__ == '__main__':
+    unittest.main()
